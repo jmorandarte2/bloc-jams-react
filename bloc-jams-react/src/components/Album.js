d3e92f3a24f8a55d5e152c3b96bb6c692969b1d8
John Alexis Morandarte <aal@Johns-MacBook-Air.local>
2018-07-14 15:14:52 -0500

    Checkpoint 8 Buttons: Completed

--- bloc-jams-react/src/components/Album.js
+++ bloc-jams-react/src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component{
   constructor (props){
@@ -68,6 +69,14 @@ class Album extends Component{
   }
 }
 
+handlePrevClick(){
+  const currentIndex = this.state.album.songs.findIndex (song => this.state.currentSong === song);
+  const newIndex = Math.max(0, currentIndex -1);
+  const newSong = this.state.album.songs[newIndex];
+  this.setSong(newSong);
+  this.play();
+}
+
   render(){
     return (
       <section className="album">
@@ -95,6 +104,10 @@ class Album extends Component{
              )}
          
            </tbody>
+           <PlayerBar isPlaying ={this.state.isPlaying} 
+           currentSong ={this.state.currentSong}
+           handleSongClick ={()=>this.handleSongClick(this.state.currentSong)}
+           handlePrevClick={()=>this.handlePrevClick()}/>
          </table>
       </section>
     );
